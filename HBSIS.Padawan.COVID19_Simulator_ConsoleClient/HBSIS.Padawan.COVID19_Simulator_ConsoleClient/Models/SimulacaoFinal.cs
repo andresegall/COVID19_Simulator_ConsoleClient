7dbce2d030c5a6003d4269b201b4f68b4e80782c
André Vinícius Segall Ando <68112091+andresegall@users.noreply.github.com>
2020-09-22 15:57:40 -0300

    Add files via upload

---
+++ HBSIS.Padawan.COVID19_Simulator_ConsoleClient/HBSIS.Padawan.COVID19_Simulator_ConsoleClient/Models/SimulacaoFinal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HBSIS.Padawan.TestandoMeuPrimeiroAPI.Models
+{
+	public class SimulacaoFinal
+	{
+		public int ID { get; set; }
+		public string Nome { get; set; }
+		public string Descricao { get; set; }
+		public DateTime Data { get; set; }
+		public IEnumerable<Estado> Contexto { get; set; }
+		public int Semanas { get; set; }
+		public int Infectados { get; set; }
+		public int Curados { get; set; }
+		public int Mortos { get; set; }
+
+		public SimulacaoFinal()
+		{
+			ID = 0;
+			Nome = "";
+			Descricao = "";
+			Data = DateTime.UtcNow;
+			Contexto = new List<Estado>();
+			Semanas = 0;
+			Infectados = 0;
+			Curados = 0;
+			Mortos = 0;
+		}
+	}
+}
